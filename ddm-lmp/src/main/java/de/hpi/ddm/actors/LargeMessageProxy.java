6a658291b33d40a3bd07f9ceb71b6797c1f40fc5
Youri K <youri.kaminsky@student.hpi.de>
2019-11-11 17:34:57 +0100

    implement slave side

--- ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
+++ ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
@@ -1,11 +1,16 @@
 package de.hpi.ddm.actors;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.*;
 
 import akka.actor.AbstractLoggingActor;
 import akka.actor.ActorRef;
 import akka.actor.ActorSelection;
 import akka.actor.Props;
+import com.twitter.chill.KryoPool;
+import de.hpi.ddm.structures.KryoPoolSingleton;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,9 +20,10 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	////////////////////////
 	// Actor Construction //
 	////////////////////////
+	private SortedSet<OrderedMessage> receivedChunks = new TreeSet<>(Comparator.comparing(OrderedMessage::getSerialNumber));
 
 	public static final String DEFAULT_NAME = "largeMessageProxy";
-	
+
 	public static Props props() {
 		return Props.create(LargeMessageProxy.class);
 	}
@@ -25,7 +31,7 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	////////////////////
 	// Actor Messages //
 	////////////////////
-	
+
 	@Data @NoArgsConstructor @AllArgsConstructor
 	public static class LargeMessage<T> implements Serializable {
 		private static final long serialVersionUID = 2940665245810221108L;
@@ -40,11 +46,18 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 		private ActorRef sender;
 		private ActorRef receiver;
 	}
-	
+
+	@Data @NoArgsConstructor @AllArgsConstructor
+	public static class OrderedMessage implements Serializable {
+		private byte[] bytes;
+		private long serialNumber;
+		private long endNumber;
+	}
+
 	/////////////////
 	// Actor State //
 	/////////////////
-	
+
 	/////////////////////
 	// Actor Lifecycle //
 	/////////////////////
@@ -52,7 +65,7 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	////////////////////
 	// Actor Behavior //
 	////////////////////
-	
+
 	@Override
 	public Receive createReceive() {
 		return receiveBuilder()
@@ -65,7 +78,7 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	private void handle(LargeMessage<?> message) {
 		ActorRef receiver = message.getReceiver();
 		ActorSelection receiverProxy = this.context().actorSelection(receiver.path().child(DEFAULT_NAME));
-		
+
 		// This will definitely fail in a distributed setting if the serialized message is large!
 		// Solution options:
 		// 1. Serialize the object and send its bytes batch-wise (make sure to use artery's side channel then).
@@ -76,7 +89,22 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	}
 
 	private void handle(BytesMessage<?> message) {
-		// Reassemble the message content, deserialize it and/or load the content from some local location before forwarding its content.
-		message.getReceiver().tell(message.getBytes(), message.getSender());
+		KryoPool kryo = KryoPoolSingleton.get();
+		OrderedMessage m = kryo.fromBytes((byte[]) message.getBytes(), OrderedMessage.class);
+		receivedChunks.add(m);
+
+		if(m.serialNumber == m.endNumber) {
+			ByteArrayOutputStream fullMessage = new ByteArrayOutputStream();
+
+			for (OrderedMessage receivedChunk : receivedChunks) {
+				try {
+					fullMessage.write(receivedChunk.getBytes());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+			message.getReceiver().tell(kryo.fromBytes(fullMessage.toByteArray()), message.getSender());
+		}
 	}
 }
