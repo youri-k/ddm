f6cb7f2ca9c0908d509f0c02283625e9325e9cb7
Youri K <youri.kaminsky@student.hpi.de>
2019-11-19 21:09:15 +0100

    implement actor logic

--- ddm-pc/src/main/java/de/hpi/ddm/actors/Master.java
+++ ddm-pc/src/main/java/de/hpi/ddm/actors/Master.java
@@ -1,9 +1,7 @@
 package de.hpi.ddm.actors;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import akka.actor.AbstractLoggingActor;
 import akka.actor.ActorRef;
@@ -13,131 +11,260 @@ import akka.actor.Terminated;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.ArrayUtils;
 
 public class Master extends AbstractLoggingActor {
 
-	////////////////////////
-	// Actor Construction //
-	////////////////////////
-	
-	public static final String DEFAULT_NAME = "master";
+    ////////////////////////
+    // Actor Construction //
+    ////////////////////////
 
-	public static Props props(final ActorRef reader, final ActorRef collector) {
-		return Props.create(Master.class, () -> new Master(reader, collector));
-	}
+    public static final String DEFAULT_NAME = "master";
 
-	public Master(final ActorRef reader, final ActorRef collector) {
-		this.reader = reader;
-		this.collector = collector;
-		this.workers = new ArrayList<>();
-	}
+    public static Props props(final ActorRef reader, final ActorRef collector) {
+        return Props.create(Master.class, () -> new Master(reader, collector));
+    }
 
-	////////////////////
-	// Actor Messages //
-	////////////////////
+    public Master(final ActorRef reader, final ActorRef collector) {
+        this.reader = reader;
+        this.collector = collector;
+        this.workers = new HashMap<>();
+        this.hashedHints = new HashMap<>();
+        this.solvedHints = new HashMap<>();
+        this.hashedPws = new HashMap<>();
+        this.tasks = new Stack<>();
+    }
 
-	@Data
-	public static class StartMessage implements Serializable {
-		private static final long serialVersionUID = -50374816448627600L;
-	}
-	
-	@Data @NoArgsConstructor @AllArgsConstructor
-	public static class BatchMessage implements Serializable {
-		private static final long serialVersionUID = 8343040942748609598L;
-		private List<String[]> lines;
-	}
+    ////////////////////
+    // Actor Messages //
+    ////////////////////
 
-	@Data
-	public static class RegistrationMessage implements Serializable {
-		private static final long serialVersionUID = 3303081601659723997L;
-	}
-	
-	/////////////////
-	// Actor State //
-	/////////////////
+    @Data
+    public static class StartMessage implements Serializable {
+        private static final long serialVersionUID = -50374816448627600L;
+    }
 
-	private final ActorRef reader;
-	private final ActorRef collector;
-	private final List<ActorRef> workers;
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class BatchMessage implements Serializable {
+        private static final long serialVersionUID = 8343040942748609598L;
+        private List<String[]> lines;
+    }
 
-	private long startTime;
-	
-	/////////////////////
-	// Actor Lifecycle //
-	/////////////////////
+    @Data
+    public static class RegistrationMessage implements Serializable {
+        private static final long serialVersionUID = 3303081601659723997L;
+    }
 
-	@Override
-	public void preStart() {
-		Reaper.watchWithDefaultReaper(this);
-	}
+    @Data
+    public static class ReadyMessage implements Serializable {
+        private static final long serialVersionUID = -90374816448627606L;
 
-	////////////////////
-	// Actor Behavior //
-	////////////////////
+    }
 
-	@Override
-	public Receive createReceive() {
-		return receiveBuilder()
-				.match(StartMessage.class, this::handle)
-				.match(BatchMessage.class, this::handle)
-				.match(Terminated.class, this::handle)
-				.match(RegistrationMessage.class, this::handle)
-				.matchAny(object -> this.log().info("Received unknown message: \"{}\"", object.toString()))
-				.build();
-	}
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class SolvedHintMessage implements Serializable {
+        private static final long serialVersionUID = -3303081601689723392L;
+        String hint;
+        String hash;
+        int[] rows;
+    }
 
-	protected void handle(StartMessage message) {
-		this.startTime = System.currentTimeMillis();
-		
-		this.reader.tell(new Reader.ReadMessage(), this.self());
-	}
-	
-	protected void handle(BatchMessage message) {
-		
-		///////////////////////////////////////////////////////////////////////////////////////////////////////
-		// The input file is read in batches for two reasons: /////////////////////////////////////////////////
-		// 1. If we distribute the batches early, we might not need to hold the entire input data in memory. //
-		// 2. If we process the batches early, we can achieve latency hiding. /////////////////////////////////
-		// TODO: Implement the processing of the data for the concrete assignment. ////////////////////////////
-		///////////////////////////////////////////////////////////////////////////////////////////////////////
-		
-		if (message.getLines().isEmpty()) {
-			this.collector.tell(new Collector.PrintMessage(), this.self());
-			this.terminate();
-			return;
-		}
-		
-		for (String[] line : message.getLines())
-			System.out.println(Arrays.toString(line));
-		
-		this.collector.tell(new Collector.CollectMessage("Processed batch of size " + message.getLines().size()), this.self());
-		this.reader.tell(new Reader.ReadMessage(), this.self());
-	}
-	
-	protected void terminate() {
-		this.reader.tell(PoisonPill.getInstance(), ActorRef.noSender());
-		this.collector.tell(PoisonPill.getInstance(), ActorRef.noSender());
-		
-		for (ActorRef worker : this.workers) {
-			this.context().unwatch(worker);
-			worker.tell(PoisonPill.getInstance(), ActorRef.noSender());
-		}
-		
-		this.self().tell(PoisonPill.getInstance(), ActorRef.noSender());
-		
-		long executionTime = System.currentTimeMillis() - this.startTime;
-		this.log().info("Algorithm finished in {} ms", executionTime);
-	}
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class SolvedCrackMessage implements Serializable {
+        private static final long serialVersionUID = -4303081601689723395L;
+        int row;
+        String PW;
+    }
 
-	protected void handle(RegistrationMessage message) {
-		this.context().watch(this.sender());
-		this.workers.add(this.sender());
-//		this.log().info("Registered {}", this.sender());
-	}
-	
-	protected void handle(Terminated message) {
-		this.context().unwatch(message.getActor());
-		this.workers.remove(message.getActor());
-//		this.log().info("Unregistered {}", message.getActor());
-	}
+    /////////////////
+    // Actor State //
+    /////////////////
+
+    private final ActorRef reader;
+    private final ActorRef collector;
+    private final Map<ActorRef, Worker.WorkMessage> workers;
+
+    private long startTime;
+
+    private final Stack<Worker.WorkMessage> tasks;
+    private final Map<String, List<Integer>> hashedHints;
+    private final Map<Integer, List<String>> solvedHints;
+    private final Map<Integer, String> hashedPws;
+    private int passwordLength;
+    private char[] characterUniverse;
+    private int numHints;
+
+    /////////////////////
+    // Actor Lifecycle //
+    /////////////////////
+
+    @Override
+    public void preStart() {
+        Reaper.watchWithDefaultReaper(this);
+    }
+
+    ////////////////////
+    // Actor Behavior //
+    ////////////////////
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(StartMessage.class, this::handle)
+                .match(BatchMessage.class, this::handle)
+                .match(Terminated.class, this::handle)
+                .match(RegistrationMessage.class, this::handle)
+                .match(SolvedHintMessage.class, this::handle)
+                .match(SolvedCrackMessage.class, this::handle)
+                .match(ReadyMessage.class, this::handle)
+                .matchAny(object -> this.log().info("Received unknown message: \"{}\"", object.toString()))
+                .build();
+    }
+
+    protected void handle(StartMessage message) {
+        this.startTime = System.currentTimeMillis();
+
+        this.reader.tell(new Reader.ReadMessage(), this.self());
+    }
+
+    protected void handle(BatchMessage message) {
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////
+        // The input file is read in batches for two reasons: /////////////////////////////////////////////////
+        // 1. If we distribute the batches early, we might not need to hold the entire input data in memory. //
+        // 2. If we process the batches early, we can achieve latency hiding. /////////////////////////////////
+        // TODO: Implement the processing of the data for the concrete assignment. ////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        if (message.getLines().isEmpty()) return;
+
+        this.log().info("Got a new batch of work of size " + message.getLines().size());
+
+        this.solvedHints.clear();
+
+        String[] lineOne = message.getLines().get(0);
+        this.characterUniverse = lineOne[2].toCharArray();
+        this.passwordLength = Integer.parseInt(lineOne[3]);
+        this.numHints = lineOne.length - 5;
+
+        for (String[] line : message.getLines()) {
+            int rowId = Integer.parseInt(line[0]);
+            this.hashedPws.put(rowId, line[4]);
+            for (int i = 5; i < line.length; i++) {
+                String hashedHint = line[i];
+                List<Integer> currentRows = this.hashedHints.get(hashedHint);
+
+                if (currentRows == null) currentRows = new ArrayList<>();
+
+                currentRows.add(rowId);
+                this.hashedHints.put(hashedHint, currentRows);
+            }
+        }
+
+        for (char c : this.characterUniverse) {
+            this.tasks.add(new Worker.WorkMessage(ArrayUtils.removeElement(characterUniverse, c), ArrayUtils.EMPTY_CHAR_ARRAY));
+        }
+
+        for (Map.Entry<ActorRef, Worker.WorkMessage> worker : this.workers.entrySet()) {
+            if (worker.getValue() == null) {
+                worker.getKey().tell(new Worker.HintMessage(this.hashedHints), this.self());
+                Worker.WorkMessage currWork = this.tasks.pop();
+                worker.getKey().tell(currWork, this.self());
+                this.workers.put(worker.getKey(), currWork);
+            }
+        }
+
+        // this.collector.tell(new Collector.CollectMessage("Processed batch of size " + message.getLines().size()), this.self());
+        // this.reader.tell(new Reader.ReadMessage(), this.self());
+    }
+
+    private void handle(ReadyMessage readyMessage) {
+        this.log().info("Received ready message from worker " + this.sender().toString());
+        this.sender().tell(new Worker.HintMessage(this.hashedHints), this.self());
+
+        if(!this.tasks.isEmpty()) {
+            Worker.WorkMessage currWork = this.tasks.pop();
+            this.sender().tell(currWork, this.self());
+            this.workers.put(this.sender(), currWork);
+        } else {
+            // TODO split work
+            this.workers.put(this.sender(), null);
+        }
+    }
+
+    protected void handle(SolvedHintMessage message) {
+        // TODO Propagate usedHashes map on Key found
+
+        this.log().info("Solved Hint " + message.getHint());
+
+        for (int i : message.getRows()) {
+            List<String> currentHints = this.solvedHints.get(i);
+            if (currentHints == null) currentHints = new ArrayList<>();
+
+            currentHints.add(message.getHint());
+            this.solvedHints.put(i, currentHints);
+            if (currentHints.size() == this.numHints)
+                this.sender().tell(new Worker.CrackMessage(i, this.hashedPws.get(i), currentHints.toArray(new String[0]), this.characterUniverse, this.passwordLength), this.self());
+        }
+
+        this.hashedHints.remove(message.getHash());
+        if (this.hashedHints.isEmpty())
+            this.reader.tell(new Reader.ReadMessage(), this.self());
+    }
+
+    protected void handle(SolvedCrackMessage message) {
+        this.log().info("Cracked PW of line " + message.getRow() + ", it's :" + message.getPW());
+        this.collector.tell(new Collector.CollectMessage(message.getPW()), this.self());
+        this.hashedPws.remove(message.getRow());
+
+        if(hashedPws.isEmpty()){
+            this.collector.tell(new Collector.PrintMessage(), this.self());
+            this.terminate();
+        }
+    }
+
+    protected void terminate() {
+        this.reader.tell(PoisonPill.getInstance(), ActorRef.noSender());
+        this.collector.tell(PoisonPill.getInstance(), ActorRef.noSender());
+
+        for (ActorRef worker : this.workers.keySet()) {
+            this.context().unwatch(worker);
+            worker.tell(PoisonPill.getInstance(), ActorRef.noSender());
+        }
+
+        this.self().tell(PoisonPill.getInstance(), ActorRef.noSender());
+
+        long executionTime = System.currentTimeMillis() - this.startTime;
+        this.log().info("Algorithm finished in {} ms", executionTime);
+    }
+
+    protected void handle(RegistrationMessage message) {
+        this.context().watch(this.sender());
+        this.sender().tell(new Worker.HintMessage(this.hashedHints), this.self());
+        if (!this.tasks.isEmpty()) {
+            Worker.WorkMessage currWork = this.tasks.pop();
+            this.sender().tell(currWork, this.self());
+            this.workers.put(this.sender(), currWork);
+        } else {
+            this.workers.put(this.sender(), null);
+            // TODO split work
+        }
+
+        this.log().info("Registered {}", this.sender());
+    }
+
+    protected void handle(Terminated message) {
+        Worker.WorkMessage unfinishedWork = this.workers.get(message.getActor());
+        this.tasks.add(unfinishedWork);
+
+        this.context().unwatch(message.getActor());
+        this.workers.remove(message.getActor());
+        this.log().info("Unregistered {}", message.getActor());
+    }
 }
--- ddm-pc/src/main/java/de/hpi/ddm/actors/Worker.java
+++ ddm-pc/src/main/java/de/hpi/ddm/actors/Worker.java
@@ -1,14 +1,13 @@
 package de.hpi.ddm.actors;
 
+import java.io.Serializable;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.List;
+import java.time.Duration;
+import java.util.*;
 
-import akka.actor.AbstractLoggingActor;
-import akka.actor.ActorRef;
-import akka.actor.PoisonPill;
-import akka.actor.Props;
+import akka.actor.*;
 import akka.cluster.Cluster;
 import akka.cluster.ClusterEvent.CurrentClusterState;
 import akka.cluster.ClusterEvent.MemberRemoved;
@@ -16,130 +15,255 @@ import akka.cluster.ClusterEvent.MemberUp;
 import akka.cluster.Member;
 import akka.cluster.MemberStatus;
 import de.hpi.ddm.MasterSystem;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 public class Worker extends AbstractLoggingActor {
 
-	////////////////////////
-	// Actor Construction //
-	////////////////////////
-	
-	public static final String DEFAULT_NAME = "worker";
+    ////////////////////////
+    // Actor Construction //
+    ////////////////////////
 
-	public static Props props() {
-		return Props.create(Worker.class);
-	}
+    public static final String DEFAULT_NAME = "worker";
 
-	public Worker() {
-		this.cluster = Cluster.get(this.context().system());
-	}
-	
-	////////////////////
-	// Actor Messages //
-	////////////////////
+    public static Props props() {
+        return Props.create(Worker.class);
+    }
 
-	/////////////////
-	// Actor State //
-	/////////////////
+    public Worker() {
+        this.cluster = Cluster.get(this.context().system());
+        this.runner = null;
 
-	private Member masterSystem;
-	private final Cluster cluster;
-	
-	/////////////////////
-	// Actor Lifecycle //
-	/////////////////////
+        this.log().info("Spawned new worker");
+    }
 
-	@Override
-	public void preStart() {
-		Reaper.watchWithDefaultReaper(this);
-		
-		this.cluster.subscribe(this.self(), MemberUp.class, MemberRemoved.class);
-	}
+    ////////////////////
+    // Actor Messages //
+    ////////////////////
 
-	@Override
-	public void postStop() {
-		this.cluster.unsubscribe(this.self());
-	}
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class WorkMessage implements Serializable {
+        private static final long serialVersionUID = 6303081601689723395L;
+        char[] permutationSet;
+        char[] prefix;
+    }
 
-	////////////////////
-	// Actor Behavior //
-	////////////////////
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class HintMessage implements Serializable {
+        private static final long serialVersionUID = -6303081601689723391L;
+        Map<String, List<Integer>> usedHashes;
+    }
 
-	@Override
-	public Receive createReceive() {
-		return receiveBuilder()
-				.match(CurrentClusterState.class, this::handle)
-				.match(MemberUp.class, this::handle)
-				.match(MemberRemoved.class, this::handle)
-				.matchAny(object -> this.log().info("Received unknown message: \"{}\"", object.toString()))
-				.build();
-	}
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class CrackMessage implements Serializable {
+        private static final long serialVersionUID = -1303081601689723393L;
+        int row;
+        String pwHash;
+        String[] hints;
+        char[] allPossibleCharacters;
+        int passwordLength;
+    }
 
-	private void handle(CurrentClusterState message) {
-		message.getMembers().forEach(member -> {
-			if (member.status().equals(MemberStatus.up()))
-				this.register(member);
-		});
-	}
+    /////////////////
+    // Actor State //
+    /////////////////
 
-	private void handle(MemberUp message) {
-		this.register(message.member());
-	}
+    private Member masterSystem;
+    private final Cluster cluster;
 
-	private void register(Member member) {
-		if ((this.masterSystem == null) && member.hasRole(MasterSystem.MASTER_ROLE)) {
-			this.masterSystem = member;
-			
-			this.getContext()
-				.actorSelection(member.address() + "/user/" + Master.DEFAULT_NAME)
-				.tell(new Master.RegistrationMessage(), this.self());
-		}
-	}
-	
-	private void handle(MemberRemoved message) {
-		if (this.masterSystem.equals(message.member()))
-			this.self().tell(PoisonPill.getInstance(), ActorRef.noSender());
-	}
-	
-	private String hash(String line) {
-		try {
-			MessageDigest digest = MessageDigest.getInstance("SHA-256");
-			byte[] hashedBytes = digest.digest(String.valueOf(line).getBytes("UTF-8"));
-			
-			StringBuffer stringBuffer = new StringBuffer();
-			for (int i = 0; i < hashedBytes.length; i++) {
-				stringBuffer.append(Integer.toString((hashedBytes[i] & 0xff) + 0x100, 16).substring(1));
-			}
-			return stringBuffer.toString();
-		}
-		catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
-			throw new RuntimeException(e.getMessage());
-		}
-	}
-	
-	// Generating all permutations of an array using Heap's Algorithm
-	// https://en.wikipedia.org/wiki/Heap's_algorithm
-	// https://www.geeksforgeeks.org/heaps-algorithm-for-generating-permutations/
-	private void heapPermutation(char[] a, int size, int n, List<String> l) {
-		// If size is 1, store the obtained permutation
-		if (size == 1)
-			l.add(new String(a));
+    private Map<String, List<Integer>> hashedHints;
+    private Thread runner;
 
-		for (int i = 0; i < size; i++) {
-			heapPermutation(a, size - 1, n, l);
+    /////////////////////
+    // Actor Lifecycle //
+    /////////////////////
 
-			// If size is odd, swap first and last element
-			if (size % 2 == 1) {
-				char temp = a[0];
-				a[0] = a[size - 1];
-				a[size - 1] = temp;
-			}
+    @Override
+    public void preStart() {
+        Reaper.watchWithDefaultReaper(this);
 
-			// If size is even, swap i-th and last element
-			else {
-				char temp = a[i];
-				a[i] = a[size - 1];
-				a[size - 1] = temp;
-			}
-		}
-	}
+        this.cluster.subscribe(this.self(), MemberUp.class, MemberRemoved.class);
+    }
+
+    @Override
+    public void postStop() {
+        this.cluster.unsubscribe(this.self());
+    }
+
+    ////////////////////
+    // Actor Behavior //
+    ////////////////////
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(CurrentClusterState.class, this::handle)
+                .match(MemberUp.class, this::handle)
+                .match(MemberRemoved.class, this::handle)
+                .match(WorkMessage.class, this::handle)
+                .match(HintMessage.class, this::handle)
+                .match(CrackMessage.class, this::handle)
+                .matchAny(object -> this.log().info("Received unknown message: \"{}\"", object.toString()))
+                .build();
+    }
+
+    private void handle(CurrentClusterState message) {
+        message.getMembers().forEach(member -> {
+            if (member.status().equals(MemberStatus.up()))
+                this.register(member);
+        });
+    }
+
+    private void handle(MemberUp message) {
+        this.register(message.member());
+    }
+
+    private void register(Member member) {
+        if ((this.masterSystem == null) && member.hasRole(MasterSystem.MASTER_ROLE)) {
+            this.masterSystem = member;
+
+            this.getContext().actorSelection(member.address() + "/user/" + Master.DEFAULT_NAME).tell(new Master.RegistrationMessage(), this.self());
+        }
+    }
+
+    private void handle(MemberRemoved message) {
+        if (this.masterSystem.equals(message.member()))
+            this.self().tell(PoisonPill.getInstance(), ActorRef.noSender());
+    }
+
+    private void handle(WorkMessage message) {
+        this.log().info("Got new work " + new String(message.getPermutationSet()));
+        ActorRef master = this.sender();
+        if (this.runner != null) this.runner.stop();
+        this.runner = new Thread(() -> {
+            List<String> allPermutations = new ArrayList<>();
+            this.heapPermutation(message.getPermutationSet(), message.getPermutationSet().length, allPermutations);
+
+            String prefix = new String(message.getPrefix());
+
+            for (String permutation : allPermutations) {
+                String hash = hash(prefix + permutation);
+                List<Integer> rows = this.hashedHints.get(hash);
+                if (rows != null) {
+                    // This is okay because the array only contains a few rowIds
+                    int[] rowArray = new int[rows.size()];
+                    for (int i = 0; i < rowArray.length; i++) rowArray[i] = rows.get(i);
+                    master.tell(new Master.SolvedHintMessage(prefix + permutation, hash, rowArray), this.self());
+                }
+            }
+
+            master.tell(new Master.ReadyMessage(), this.self());
+        });
+        this.runner.start();
+    }
+
+    private void handle(HintMessage message) {
+        this.hashedHints = message.getUsedHashes();
+    }
+
+    private void handle(CrackMessage message) {
+        this.log().info("Got new crack request");
+        ActorRef master = this.sender();
+        Character[] usedChars = this.getUsedChars(message.getAllPossibleCharacters(), message.getHints());
+
+        List<String> allCombinations = new ArrayList<>();
+        generateCombinations(usedChars, "", usedChars.length, message.getPasswordLength(), allCombinations);
+        for (String oneCombination : allCombinations) {
+            if (hash(oneCombination).equals(message.getPwHash())) {
+                master.tell(new Master.SolvedCrackMessage(message.getRow(), oneCombination), this.self());
+                return;
+            }
+        }
+    }
+
+    private String hash(String line) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hashedBytes = digest.digest(String.valueOf(line).getBytes("UTF-8"));
+
+            StringBuffer stringBuffer = new StringBuffer();
+            for (int i = 0; i < hashedBytes.length; i++) {
+                stringBuffer.append(Integer.toString((hashedBytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            return stringBuffer.toString();
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    // Generating all permutations of an array using Heap's Algorithm
+    // https://en.wikipedia.org/wiki/Heap's_algorithm
+    // https://www.geeksforgeeks.org/heaps-algorithm-for-generating-permutations/
+    private void heapPermutation(char[] a, int size, List<String> l) {
+        // If size is 1, store the obtained permutation
+        if (size == 1)
+            l.add(new String(a));
+
+        for (int i = 0; i < size; i++) {
+            heapPermutation(a, size - 1, l);
+
+            // If size is odd, swap first and last element
+            if (size % 2 == 1) {
+                char temp = a[0];
+                a[0] = a[size - 1];
+                a[size - 1] = temp;
+            }
+
+            // If size is even, swap i-th and last element
+            else {
+                char temp = a[i];
+                a[i] = a[size - 1];
+                a[size - 1] = temp;
+            }
+        }
+    }
+
+    private void generateCombinations(Character[] set, String prefix, int n, int k, List<String> l) {
+
+        // Base case: k is 0,
+        // print prefix
+        if (k == 0) {
+            l.add(prefix);
+            return;
+        }
+
+        // One by one add all characters
+        // from set and recursively
+        // call for k equals to k-1
+        for (int i = 0; i < n; ++i) {
+
+            // Next character of input added
+            String newPrefix = prefix + set[i];
+
+            // k is decreased, because
+            // we have added a new character
+            generateCombinations(set, newPrefix, n, k - 1, l);
+        }
+    }
+
+    private Character[] getUsedChars(char[] characterUniverse, String[] hints) {
+        Set<Character> inPw = new HashSet<>();
+        List<Character> notInPw = new ArrayList<>();
+
+        for (String hint : hints) {
+            for (char c : characterUniverse) inPw.add(c);
+            for (char c : hint.toCharArray()) inPw.remove(c);
+
+            notInPw.addAll(inPw);
+            inPw.clear();
+        }
+
+        List<Character> usedChars = new ArrayList<>();
+        for(char c : characterUniverse) usedChars.add(c);
+        usedChars.removeAll(notInPw);
+
+        return usedChars.toArray(new Character[0]);
+    }
 }
