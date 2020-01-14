f8402e972e5f1df181749698d3c84c5f604c3ff5
rschwanhold <r.schwanhold@hotmail.de>
2020-01-14 17:44:04 +0100

    add code for foreign key relationships

---
+++ spark-foreign-key/src/main/scala/de/hpi/spark_tutorial/SimpleSpark.scala
@@ -0,0 +1,61 @@
+package de.hpi.spark_tutorial
+
+import org.apache.spark.sql.{Dataset, Encoder, SparkSession}
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.DecisionTreeClassificationModel
+import org.apache.spark.ml.classification.DecisionTreeClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer}
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+
+// A Scala case class; works out of the box as Dataset type using Spark's implicit encoders
+case class Person(name:String, surname:String, age:Int)
+
+// A non-case class; requires an encoder to work as Dataset type
+class Pet(var name:String, var age:Int) {
+  override def toString = s"Pet(name=$name, age=$age)"
+}
+
+object SimpleSpark extends App {
+
+  override def main(args: Array[String]): Unit = {
+    // Turn off logging
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+
+    //------------------------------------------------------------------------------------------------------------------
+    // Setting up a Spark Session
+    //------------------------------------------------------------------------------------------------------------------
+
+    // Create a SparkSession to work with Spark
+    val sparkBuilder = SparkSession
+      .builder()
+      .appName("SparkTutorial")
+      .master("local[4]") // local, with 4 worker cores
+    val spark = sparkBuilder.getOrCreate()
+
+    // Set the default number of shuffle partitions (default is 200, which is too high for local deployment)
+    spark.conf.set("spark.sql.shuffle.partitions", "8") //
+
+    // Importing implicit encoders for standard library classes and tuples that are used as Dataset types
+    import spark.implicits._
+
+    def time[R](block: => R): R = {
+      val t0 = System.currentTimeMillis()
+      val result = block
+      val t1 = System.currentTimeMillis()
+      println(s"Execution: ${t1 - t0} ms")
+      result
+    }
+
+    //------------------------------------------------------------------------------------------------------------------
+    // Inclusion Dependency Discovery (Homework)
+    //------------------------------------------------------------------------------------------------------------------
+
+    val inputs = List("region", "nation", "supplier", "customer", "part", "lineitem", "orders")
+      .map(name => s"data/TPCH/tpch_$name.csv")
+
+    time {Sindy.discoverINDs(inputs, spark)}
+  }
+}
